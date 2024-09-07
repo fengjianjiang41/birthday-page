e027f57ef3fc35832dc72b422cdb97b3c07130fa
xueshenjiang <1689899256@qq.com>
2024-09-07 23:44:12 +0800

    Milestone 2

--- script.js
+++ script.js
@@ -98,37 +98,38 @@ function checkKey() {
         secretKeyInput.style.display = "none";
         document.getElementById("key-button").style.display = "none";
         keyWarning.innerHTML = "";
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-              console.log(position);
-              lon = position.coords.longitude;
-              lat = position.coords.latitude; 
-              const keycry = key;           
-              // API ID
-              const api = "c05f892937b2d1a3ffd7180a2e667529";
-            
-              // API URL
-              const base =
-              `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
-              `lon=${lon}&appid=${api}`;
-            
-              // Calling the API
-              fetch(base)
-                .then((response) => {
-                  return response.json();
-                })
-                .then((data) => {
-                  console.log(data);
-                  temperature.textContent = 
-                      Math.floor(data.main.temp - kelvin) + "°C";
-                  summary.textContent = data.weather[0].description;
-                  loc.textContent = data.name + "," + data.sys.country;
-                  let icon1 = data.weather[0].icon;
-                  icon.innerHTML = 
-                      `<img src="icons/${icon1}.svg" style= 'height:10rem'/>`;
-                });
-            });
-              }
+        // if (navigator.geolocation) {
+        //     navigator.geolocation.getCurrentPosition((position) => {
+        //       console.log(position);
+        //       lon = position.coords.longitude;
+        //       lat = position.coords.latitude; 
+        //       const keycry = key;           
+        //       // API ID
+        const api = "c05f892937b2d1a3ffd7180a2e667529";
+    
+        // API URL
+        const base =
+        `https://api.openweathermap.org/data/2.5/weather?lat=39.9&` +
+        `lon=116.4&appid=${api}`;
+    
+        // Calling the API
+        fetch(base)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            temperature.textContent = 
+                Math.floor(data.main.temp - kelvin) + "°C";
+            summary.textContent = data.weather[0].description;
+            loc.textContent = data.name + "," + data.sys.country;
+            let icon1 = data.weather[0].icon;
+            icon.innerHTML = 
+                `<img src="icons/${icon1}.svg" style= 'height:10rem'/>`;
+        })
+        
+        // );
+        //       }
     } else {
         keyWarning.innerHTML = "错误哦，是生日还是秘钥搞错啦？";
         secretKeyInput.value = "";
